88e1381b153132066e03d180479e5e79be835f43
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-04 15:39:29 +0200

    remove input from app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css'
-import InputFormField from './components/InputFormField/InputFormField'
 
 function App() {
 
   return (
     <>
-      <InputFormField label='Username' id='username' inputType='text' placeholder='petar@gmail.com' onChange={()=>{}} value=''/>
-      <InputFormField label='Password' id='password' inputType='password' onChange={() => {}} value=''/>
     </>
   )
 }
