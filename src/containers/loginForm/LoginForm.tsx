9857d8b581561d25e54a62e03ccf0d9f6640dbaf
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-05 13:09:24 +0200

    removed unnecessary input from react

--- src/containers/loginForm/LoginForm.tsx
+++ src/containers/loginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react"
+import { useState } from "react"
 import './LoginForm.css'
 import InputFormField from "../../components/inputFormField/InputFormField";
 import Button from "../../components/button/Button";
