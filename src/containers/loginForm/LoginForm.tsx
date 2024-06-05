b5d07b9da4f029fe370c65bd89484800348a769b
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-05 16:23:51 +0200

    created events for focus and focus out on inputs, also created error states separately for username and password

--- src/containers/loginForm/LoginForm.tsx
+++ src/containers/loginForm/LoginForm.tsx
@@ -8,7 +8,8 @@ const LoginForm = () => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [hasBackendError, setHasBackendError] = useState<boolean>(false);
-    const [hasError, setHasError] = useState<boolean>(false);
+    const [hasPasswordError, setHasPasswordError] = useState<boolean>(false);
+    const [hasUsernameError, setHasUsernameError] = useState<boolean>(false);
 
     const validateInputFields = (username:string, password: string) => {
       if (!username || !password) {
@@ -17,15 +18,29 @@ const LoginForm = () => {
       return true;
     }
 
+    const handleUsernameOnBlur = () => {
+      if(!username){
+        setHasUsernameError(true)
+      }
+    }
+
+    const handlePasswordOnBlur = () => {
+      if(!password){
+        setHasPasswordError(true)
+      }
+    }
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 
       e.preventDefault();
       if(validateInputFields(username, password)){
         setUsername('');
         setPassword('');
-        setHasError(false);
+        setHasPasswordError(false);
+        setHasUsernameError(false);
       }else{
-        setHasError(true);
+        setHasPasswordError(true);
+        setHasUsernameError(true);
       }
     }
 
@@ -34,16 +49,36 @@ const LoginForm = () => {
     <form className="login-form" onSubmit={handleSubmit}>
       <h1 className="login-form__title">Login</h1>
       <div className="login-form__inputs">
-      <InputFormField label="Username" placeholder="username@mail.com" id="username" inputType="email" value={username} onChange={(e) => setUsername(e.target.value)}/>
-      <InputFormField label="Password" placeholder="*********" id="password" value={password} inputType="password" onChange={(e) => setPassword(e.target.value)}/>
-      {
-        hasError && 
-        <p className="login-form__error">All fields are required</p>
-      }
-      {
-        hasBackendError && 
-        <p className="login-form__error">Wrong credentials</p>
-      }
+        <InputFormField 
+          label="Username" 
+          className={hasUsernameError || hasBackendError ? 'red-border-bottom' : ''} 
+          placeholder="username@mail.com" 
+          id="username" 
+          inputType="email" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+          onFocus={() => setHasUsernameError(false)} 
+          onBlur={handleUsernameOnBlur}
+        />
+        <InputFormField 
+          label="Password" 
+          className={hasPasswordError || hasBackendError ? 'red-border-bottom' : ''} 
+          placeholder="*********" 
+          id="password" 
+          value={password} 
+          inputType="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          onFocus={() => setHasPasswordError(false)} 
+          onBlur={handlePasswordOnBlur}
+        />
+        {
+          (hasUsernameError || hasPasswordError) && 
+          <p className="login-form__error">All fields are required</p>
+        }
+        {
+          hasBackendError && 
+          <p className="login-form__error">Wrong credentials</p>
+        }
       </div>
       <Button title="Login" type="submit" className="login-form__button"/>
     </form>
