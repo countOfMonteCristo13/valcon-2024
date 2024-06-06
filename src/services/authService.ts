b7ef86e037742364c67c29f996481c06182d7b48
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-06 11:28:50 +0200

    created auth service and login function

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
 import { LoginFormData } from "../models/LoginFormData";
 import { AuthRequestModel } from "../models/AuthModels";
+import { LoginResponseData } from "../models/LocalStorageData";
 
-const authEndpoint = 'auth/token'
+const authEndpoint = '/auth/token'
 
-export const handleLogin = async (formData: LoginFormData) => {
+export const login = async (formData: LoginFormData) => {
     const requestBody: AuthRequestModel = {
         usernameOrEmail: formData.username,
         password: formData.password,
         rememberMe: true
     }
 
-    await axios.post(`${import.meta.env.BASE_URL + authEndpoint}`,requestBody).then((response) => {
-        console.log(response);
-    })
+    return axios.post<LoginResponseData>(`${import.meta.env.VITE_BASE_URL + authEndpoint}`,requestBody).then((response) => {
+        return response.data;
+    });
 
 }
