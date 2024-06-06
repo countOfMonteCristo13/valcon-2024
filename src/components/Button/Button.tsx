39f78b1534a1b5d9ed53753586fa44edb01f20da
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-06 15:59:50 +0200

    fixed changes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,7 @@ import './Button.css'
 type ButtonProps = {
     title:string;
     type?: "submit" | "reset" | "button" | undefined;
-<<<<<<< HEAD
     onClick?: () => void;
-=======
-    onClick: () => void;
->>>>>>> 2e6b99e (button component created)
     className?: string;
     disabled?: boolean
 }
