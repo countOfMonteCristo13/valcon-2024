dd434b1a2e973a2e5a7c3b6d6672e3fa335355e4
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-06 11:31:31 +0200

    added routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import LoginPage from './pages/loginPage/LoginPage'
+import NotFoundPage from './pages/notFoundPage/NotFoundPage'
+
 
 function App() {
 
   return (
     <main className='props-app'>
-      <LoginPage/>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='*' element={<NotFoundPage/>}/>
+      </Routes>
     </main>
   )
 }
