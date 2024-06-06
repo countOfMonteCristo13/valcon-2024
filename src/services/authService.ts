146f4b1efde7019bce44155bdfc0bdce4cf45991
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-06 14:52:18 +0200

    created refresh token function

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,19 +1,40 @@
 import axios from "axios";
 import { LoginFormData } from "../models/LoginFormData";
-import { AuthRequestModel } from "../models/AuthModels";
-import { LoginResponseData } from "../models/LocalStorageData";
+import { AuthRequestData } from "../models/AuthData";
+import { TokenResponseData } from "../models/LocalStorageData";
+import { getDataFromLocalStorage } from "./localStorageService";
 
-const authEndpoint = '/auth/token'
+const authUrl = '/auth/token'
+const refreshTokenUrl = '/auth/refresh-token'
 
 export const login = async (formData: LoginFormData) => {
-    const requestBody: AuthRequestModel = {
+    const requestBody: AuthRequestData = {
         usernameOrEmail: formData.username,
         password: formData.password,
         rememberMe: true
     }
 
-    return axios.post<LoginResponseData>(`${import.meta.env.VITE_BASE_URL + authEndpoint}`,requestBody).then((response) => {
+    return axios.post<TokenResponseData>(`${import.meta.env.VITE_BASE_URL + authUrl}`,requestBody).then((response) => {
         return response.data;
     });
 
+}
+
+export const refreshToken = async () => {
+    const refreshToken: string | null = getDataFromLocalStorage('refreshToken')
+
+    if (!refreshToken) {
+        throw new Error('Refresh token is not available');
+    }
+
+    try {
+        return axios.post<TokenResponseData>(
+            `${import.meta.env.VITE_BASE_URL + refreshTokenUrl}`,
+            { refreshToken }
+        ).then(response => {
+            return response.data;
+        })
+    } catch (error) {
+        throw new Error('Error while refreshing token');
+    }
 }
