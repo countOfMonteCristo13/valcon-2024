4ed0efe86eb25d3f28bcb398739879725c95b23b
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-07 16:57:32 +0200

    modified landing page by adding infinite scroll

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -3,41 +3,61 @@ import PropList from '../../components/propList/PropList'
 import useProps from '../../hooks/useProps';
 import './LandingPage.css'
 import Button from '../../components/button/Button';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const LandingPage = () => {
 
-    const [size,setSize] = useState(5);
+    const [size,setSize] = useState(10);
     const [page,setPage] = useState(0)
     const sort = 'asc'
 
-    const {propsList, isLoading, hasError} = useProps(page,size,sort);
+    const {propsList, isLoading, hasError, totalElements} = useProps(page,size,sort);
 
     const nextPage = () => {
-        setPage(page + 1);
+        setPage(prevPage => prevPage + 1);
     };
 
     const prevPage = () => {
         if (page > 0) {
-            setPage(page - 1);
+            setPage(prevPage => prevPage - 1);
         }
     };
 
-    if(isLoading){
-        return (
-            <h1>Loading</h1>
-        )
-    }
+    // if(isLoading){
+    //     return (
+    //         <h1>Loading</h1>
+    //     )
+    // }
+
 
   return (
     <div className='landing-page'>
-        <div className='landing-page__props'>
-            <PropList propList={propsList}/>
-            <div className='landing-page__props__pagination'>
-                <Button title='Previous' onClick={prevPage} className='landing-page__props__pagination__button'/>
-                <Button title='Next' onClick={nextPage} className='landing-page__props__pagination__button'/>
-            </div>
+            <InfiniteScroll
+                className='landing-page__props'
+                dataLength={propsList.length} //This is important field to render the next data
+                next={() => setSize(prevSize => prevSize + 1)}
+                hasMore={size <= totalElements}
+                loader={<h4>Loading...</h4>}
+                endMessage={
+                <p style={{ textAlign: 'center' }}>
+                <b>Yay! You have seen it all</b>
+                </p>
+                
+                }
+                scrollableTarget={'landing-page'}
+                refreshFunction={() => setSize(10)}
+                pullDownToRefresh
+                pullDownToRefreshThreshold={50}
+                pullDownToRefreshContent={
+                  <h3 className='full-width'>&#8595; Pull down to refresh</h3>
+                }
+                releaseToRefreshContent={
+                  <p className='full-width'>&#8593; Release to refresh</p>
+                }
+            >
+                <PropList propList={propsList}/>
+            </InfiniteScroll>
         </div>
-    </div>
   )
 }
 
