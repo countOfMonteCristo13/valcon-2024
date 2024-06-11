20d2d652074b2da585b4db8bbea941a6ff441b40
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-11 11:00:50 +0200

    Created rewards and profile page

---
+++ src/pages/profilePage/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import MenuBar from '../../containers/menuBar/MenuBar'
+import './ProfilePage.css'
+
+const ProfilePage = () => {
+  return (
+    <div>
+        <MenuBar/>
+    </div>
+  )
+}
+
+export default ProfilePage
---
+++ src/pages/rewardsPage/RewardsPage.tsx
@@ -0,0 +1,12 @@
+import MenuBar from '../../containers/menuBar/MenuBar'
+import './RewardsPage.css'
+
+const RewardsPage = () => {
+  return (
+    <div>
+      <MenuBar/>
+    </div>
+  )
+}
+
+export default RewardsPage
