fd6f11e07d70d1fe06891f3be59cc65b5e545d7b
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-11 11:02:12 +0200

    modified menu bar to be more functional for mobile and larger devices

--- src/containers/menuBar/MenuBar.tsx
+++ src/containers/menuBar/MenuBar.tsx
@@ -10,45 +10,61 @@ import './MenuBar.css'
 
 
 const MenuBar = () => {
-    const [isMenuBarExpanded, setIsMenuBarExpanded] = useState<boolean>(true);
+    const [isMenuBarExpanded, setIsMenuBarExpanded] = useState<boolean>(false);
     const {isShown} = useScrollDown();
 
+    const isActiveLink = (isActive:boolean) => {
+        return {
+            color: isActive ? '#ffae00' : ''
+        }
+    }
 
   return (
-    <nav className={`menu-bar ${(!isShown && window.innerWidth < 769) && 'hidden'} ${isMenuBarExpanded && 'menu-bar-expanded'}`}>
-        <div className='menu-bar__logo'>
-            <img className='menu-bar__logo__image' src={logo} alt="logo" />
-            <h2 className={`menu-bar__logo__title ${isMenuBarExpanded && 'show'}`}>Props</h2>
-        </div>
-        <div className='menu-bar__links'>
-            <NavLink to={'/'} className={`menu-bar__links__link ${isMenuBarExpanded && 'menu-bar__links__link-expanded'}`}>
-                <BiHome size={32}/>
-                <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Home</h3>
-            </NavLink>
-            <NavLink to={'/rewards'} className='menu-bar__links__link'>
-                <BiGift size={32}/>
-                <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Rewards</h3>
-            </NavLink>
-            <NavLink to={'/sad'} className='menu-bar__links__link'>
-                <BiGift size={32}/>
-                <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Home</h3>
-            </NavLink>
-            <NavLink to={'/sad'} className='menu-bar__links__link menu-bar__link__profile'>
-                <RxAvatar size={32}/>
-                <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Profile</h3>
-            </NavLink>
-            
-        </div>
-        <div className='menu-bar__profile'>
-            <div onClick={() => setIsMenuBarExpanded(!isMenuBarExpanded)}>
-                <FaArrowRight size={32}/>
+    <nav className={`menu-bar__wrapper ${isMenuBarExpanded && 'menu-bar-expanded'}`}>
+
+        <div className={`menu-bar__small ${!isShown && 'hidden'} `}>
+            <div className='menu-bar__small__links'>
+                <NavLink to={'/'} className='menu-bar__small__links__link' style={({isActive}) => isActiveLink(isActive)}>
+                    <BiHome size={32}/>
+                    <h3 className='menu-bar__small__links__link__title'>Home</h3>
+                </NavLink>
+                <NavLink to={'/rewards'} className='menu-bar__small__links__link' style={({isActive}) => isActiveLink(isActive)}>
+                    <BiGift size={32}/>
+                    <h3 className='menu-bar__small__links__link__title' >Rewards</h3>
+                </NavLink>
+                <NavLink to={'/profile'} className='menu-bar__small__links__link' style={({isActive}) => isActiveLink(isActive)}>
+                    <RxAvatar size={32}/>
+                    <h3 className='menu-bar__small__links__link__title'>Profile</h3>
+                </NavLink>
             </div>
-            <NavLink to={'/sad'} className='menu-bar__links__link'>
-                <RxAvatar size={32}/>
-                <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Profile</h3>
-            </NavLink>
         </div>
 
+
+        <div className={`menu-bar ${isMenuBarExpanded && 'menu-bar-expanded'}`}>
+            <div className='menu-bar__logo'>
+                <img className='menu-bar__logo__image' src={logo} alt="logo" />
+                <h2 className={`menu-bar__logo__title ${isMenuBarExpanded && 'show'}`}>Props</h2>
+            </div>
+            <div className='menu-bar__links'>
+                <NavLink to={'/'} className={`menu-bar__links__link`} style={({isActive}) => isActiveLink(isActive)}>
+                    <BiHome size={32}/>
+                    <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Home</h3>
+                </NavLink>
+                <NavLink to={'/rewards'} className='menu-bar__links__link' style={({isActive}) => isActiveLink(isActive)}>
+                    <BiGift size={32}/>
+                    <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Rewards</h3>
+                </NavLink>
+            </div>
+            <div className='menu-bar__profile'>
+                <div  onClick={() => setIsMenuBarExpanded(!isMenuBarExpanded)}>
+                    <FaArrowRight size={32}/>
+                </div>
+                <NavLink to={'/profile'} className='menu-bar__links__link' style={({isActive}) => isActiveLink(isActive)}>
+                    <RxAvatar size={32}/>
+                    <h3 className={`menu-bar__links__link__title ${isMenuBarExpanded && 'show'}`}>Profile</h3>
+                </NavLink>
+            </div>
+        </div>
     </nav>
   )
 }
