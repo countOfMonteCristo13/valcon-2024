2ad1a63e2482ba55c0d6798af421e3420ebd3b3a
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-11 17:06:53 +0200

    created components for side bar on landing page

---
+++ src/containers/randomRewards/RandomRewards.tsx
@@ -0,0 +1,14 @@
+import './RandomRewards.css'
+
+const RandomRewards = () => {
+  return (
+    <div className='random-rewards'>
+      <p className='reward-1'>reward</p>
+      <p className='reward-2'>reward</p>
+      <p className='reward-3'>reward</p>
+      <p className='reward-4'>reward</p>
+    </div>
+  )
+}
+
+export default RandomRewards
---
+++ src/containers/userPoints/UserPoints.tsx
@@ -0,0 +1,13 @@
+import './UserPoints.css'
+
+const UserPoints = () => {
+  return (
+    <div className='user-points'>
+      <p>points to use</p>
+      <p>total points given</p>
+      <p>total points</p>
+    </div>
+  )
+}
+
+export default UserPoints
