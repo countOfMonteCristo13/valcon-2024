ff0f50cbcc94cac06df1f31174d4e0a5156a7902
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-12 10:30:32 +0200

    created function for adding new prop

--- src/services/PropsService.ts
+++ src/services/PropsService.ts
@@ -1,5 +1,7 @@
 import { GetPropsRequestBody } from "../models/request/GetPropsRequest";
+import { PostPropRequest } from "../models/request/PostPropRequest";
 import { GetPropsResponse } from "../models/response/GetPropsResponse";
+import { PostPropResponse } from "../models/response/PostPropResponse";
 import { axiosInterceptor } from "./AxiosInterceptor";
 
 export const fetchProps = async ({page,size,sort}: GetPropsRequestBody): Promise<GetPropsResponse> => {
@@ -12,3 +14,9 @@ export const fetchProps = async ({page,size,sort}: GetPropsRequestBody): Promise
   });
   return response.data;
 };
+
+export const addProp = async (newPropRequestBody: PostPropRequest): Promise<PostPropResponse> => {
+  const response = await axiosInterceptor.post<PostPropResponse>('/props',newPropRequestBody);
+
+  return response.data;
+}
