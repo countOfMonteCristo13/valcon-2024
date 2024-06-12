343b8ef78320f82b54b19bbc4c1669ae9c48c0cb
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-12 11:14:47 +0200

    created interface for responses

---
+++ src/models/response/Response.ts
@@ -0,0 +1,15 @@
+import { Pageable } from "../Pageable";
+import { Sort } from "../Sort";
+
+export interface Response{
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    pageable: Pageable;
+    numberOfElements: number;
+    first: boolean;
+    size: number;
+    number: number;
+    sort: Sort;
+    empty:boolean;
+}
