fce8d3559eeec2adeeb0a7a3671b449c32e39000
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-13 17:15:00 +0200

    created styles for user suggestion card and updated component to show generic picture when there is no actual one

--- src/components/userSuggestionCard/UserSuggestionCard.tsx
+++ src/components/userSuggestionCard/UserSuggestionCard.tsx
@@ -1,14 +1,22 @@
+import { RxAvatar } from "react-icons/rx";
 import { PropsUser } from "../../models/PropsData"
 import { IMAGE_BASE_URL } from "../../utils/constants";
+import './UserSuggestionCard.css'
 
 type UserSuggestionCardProps = {
     user:PropsUser;
+    onClick: (userId: number, firstName: string, lastName: string) => void;
 }
 
-const UserSuggestionCard = ({user}:UserSuggestionCardProps) => {
+const UserSuggestionCard = ({user, onClick}:UserSuggestionCardProps) => {
   return (
-    <div className='user-suggestion-card'>
+    <div className='user-suggestion-card' onClick={() => onClick(user.id, user.firstName, user.lastName)}>
+      {
+        user.profileImageReference ?
       <img src={`${IMAGE_BASE_URL}${user.profileImageReference}`} alt="profile-image" className="user-suggestion-card__img"/>
+      :
+      <RxAvatar size={32}/>
+      }
       <p className="user-suggestion-card__name">{user.firstName} {user.lastName}</p>
     </div>
   )
