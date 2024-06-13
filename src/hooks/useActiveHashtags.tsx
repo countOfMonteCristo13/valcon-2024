eb4ec362f8b1705d72b32d91fdb3706a98acdec0
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-13 17:13:24 +0200

    created hook for fetching hashtags

---
+++ src/hooks/useActiveHashtags.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { PageableResponse } from '../models/response/Response';
+import { Hashtag } from '../models/PropsData';
+import { fetchActiveHashtags } from '../services/HashtagsService';
+
+const useActiveHashtags = (page: number, size: number, sort: string, search:string) => {
+  const [activeHashtagsList, setActiveHashtagsList] = useState<Hashtag[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [hasError, setHasError] = useState(null);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      setIsLoading(true);
+      try {
+        const response: PageableResponse<Hashtag> = await fetchActiveHashtags({page, size, sort, search});
+        if(page === 0){
+          setActiveHashtagsList(response.content);
+        }else{
+          setActiveHashtagsList((prevList) => [...prevList,...response.content]);
+        }
+        setTotalPages(response.totalPages);
+      } catch (error:any) {
+        setHasError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    
+    if (search.length > 1) {
+      fetchList();
+    } else {
+      setActiveHashtagsList([]);
+    }
+    
+  }, [page, sort, size, search]); 
+
+  return { activeHashtagsList, isLoading, hasError, totalPages };
+};
+
+export default useActiveHashtags;
