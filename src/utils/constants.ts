5f25758635c6d8bd066a418fb658b4ff80b76c27
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-13 16:58:35 +0200

    updated constant for primary color and added secondary color

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
 export const LAPTOP_WIDTH_SIZE = 1024;
 
-export const ACTIVE_COLOR = '#ffae00';
+export const PRIMARY_COLOR = '#ffae00';
+
+export const SECONDARY_COLOR = '#212121';
 
 export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
