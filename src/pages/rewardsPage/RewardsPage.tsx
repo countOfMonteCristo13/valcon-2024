be055ec3d27740466177c942656c9f8d82072d66
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-14 10:07:44 +0200

    added hook for getting rewards and displaying them on page

--- src/pages/rewardsPage/RewardsPage.tsx
+++ src/pages/rewardsPage/RewardsPage.tsx
@@ -1,12 +1,27 @@
+import RewardCard from "../../components/rewardCard/RewardCard";
+import UseRewards from "../../hooks/useRewards";
 import MenuBar from "../../layouts/menuBar/MenuBar";
 import "./RewardsPage.css";
 
 const RewardsPage = () => {
 
+  const {
+    rewards,
+    isLoading: areRewardsLoading,
+    hasError: hasRewardsError,
+  } = UseRewards();
+
   return (
     <div className="rewards-page__layout">
       <MenuBar />
       <div className="rewards-page">
+        {rewards && (
+          <div className="rewards-page__all-rewards">
+            {rewards.map((reward) => (
+              <RewardCard reward={reward} />
+            ))}
+          </div>
+        )}
         <div className="rewards-page__my-rewards">cao</div>
         <div className="rewards-page__some-rewards">hello</div>
       </div>
