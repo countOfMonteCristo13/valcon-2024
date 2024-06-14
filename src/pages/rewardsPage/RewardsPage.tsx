db13bb41d7ab3794a25fe8754b759e6d4227817b
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-14 09:15:30 +0200

    created layout for rewards page

--- src/pages/rewardsPage/RewardsPage.tsx
+++ src/pages/rewardsPage/RewardsPage.tsx
@@ -1,12 +1,17 @@
-import MenuBar from '../../layouts/menuBar/MenuBar'
-import './RewardsPage.css'
+import MenuBar from "../../layouts/menuBar/MenuBar";
+import "./RewardsPage.css";
 
 const RewardsPage = () => {
-  return (
-    <div>
-      <MenuBar/>
-    </div>
-  )
-}
 
-export default RewardsPage
+  return (
+    <div className="rewards-page__layout">
+      <MenuBar />
+      <div className="rewards-page">
+        <div className="rewards-page__my-rewards">cao</div>
+        <div className="rewards-page__some-rewards">hello</div>
+      </div>
+    </div>
+  );
+};
+
+export default RewardsPage;
