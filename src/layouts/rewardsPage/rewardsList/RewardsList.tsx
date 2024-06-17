ba0bd50ae882a5dd1fd330dd7893d0dceaef2675
CountMonteCristo13 <buza95@gmail.com>
2024-06-17 08:11:14 +0200

    updated list componet for rewards

--- src/layouts/rewardsPage/rewardsList/RewardsList.tsx
+++ src/layouts/rewardsPage/rewardsList/RewardsList.tsx
@@ -1,24 +1,32 @@
-import { Reward } from '../../../models/RewardsData'
-import RewardCard from '../rewardCard/RewardCard';
-import './RewardsList.css'
+import { Reward } from "../../../models/RewardsData";
+import RewardCard from "../rewardCard/RewardCard";
+import "./RewardsList.css";
 
 type RewardsListProps = {
-    list: Reward[];
-    className?: string;
-    isMyRewardCard?:boolean;
-    reedemablePoints?:number;
-}
+  list: Reward[];
+  className?: string;
+  redeemablePoints?: number;
+  rewardCardRedeemReward: (id:number) => void;
+};
 
-const RewardsList = ({list, className, isMyRewardCard = false, reedemablePoints}: RewardsListProps) => {
+const RewardsList = ({
+  list,
+  className,
+  redeemablePoints,rewardCardRedeemReward
+}: RewardsListProps) => {
   return (
     <div className={className}>
-      {
-        list.map(card => (
-            <RewardCard reward={card} key={card.id} isMyReward={isMyRewardCard} reedmeablePoints={reedemablePoints}/>
-        ))
-      }
+      {list.map((card) => (
+        <RewardCard
+          reward={card}
+          key={card.id}
+          reedmeablePoints={redeemablePoints}
+          redeemReward={rewardCardRedeemReward}
+          
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default RewardsList
+export default RewardsList;
