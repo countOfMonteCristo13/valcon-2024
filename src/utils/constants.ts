26169c1113325783b88a09af8d1c7c448425532d
CountMonteCristo13 <buza95@gmail.com>
2024-06-17 08:00:14 +0200

    added two constants for tablet width and tertiary color

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,11 @@
 export const LAPTOP_WIDTH_SIZE = 1024;
 
-export const PRIMARY_COLOR = '#ffae00';
+export const TABLET_WIDTH_SIZE = 768;
 
-export const SECONDARY_COLOR = '#212121';
+export const PRIMARY_COLOR = "#ffae00";
+
+export const SECONDARY_COLOR = "#212121";
+
+export const TERTIARY_COLOR = "#2b2b2b";
 
 export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
