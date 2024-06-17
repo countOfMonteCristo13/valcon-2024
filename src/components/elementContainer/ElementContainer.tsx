7e1d00aa03a9954a5f998f54d8603ef6ba81c31e
CountMonteCristo13 <buza95@gmail.com>
2024-06-17 07:59:03 +0200

    added onClick function prop to element container

--- src/components/elementContainer/ElementContainer.tsx
+++ src/components/elementContainer/ElementContainer.tsx
@@ -1,17 +1,22 @@
-import { ReactNode } from 'react'
-import './ElementContainer.css'
+import { ReactNode } from "react";
+import "./ElementContainer.css";
 
 type ElementContainerProps = {
   children: ReactNode;
   className?: string;
-}
+  onClick?: () => void;
+};
 
-const ElementContainer = ({children,className}: ElementContainerProps) => {
+const ElementContainer = ({
+  children,
+  className,
+  onClick,
+}: ElementContainerProps) => {
   return (
-    <div className={`element-container ${className}`}>
+    <div className={`element-container ${className}`} onClick={onClick}>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default ElementContainer
+export default ElementContainer;
