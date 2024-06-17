1cdf95ecd23bd469ec487b2533226931d9659bb5
CountMonteCristo13 <buza95@gmail.com>
2024-06-17 08:15:21 +0200

    updated handle redeem function, added handler for resizing and added section for mobile devices

--- src/pages/rewardsPage/RewardsPage.tsx
+++ src/pages/rewardsPage/RewardsPage.tsx
@@ -1,56 +1,110 @@
-import UseRewards from "../../hooks/useRewards";
-import MenuBar from "../../layouts/menuBar/MenuBar";
-import "./RewardsPage.css";
-import RewardsList from "../../layouts/rewardsPage/rewardsList/RewardsList";
-import { BiCoin } from "react-icons/bi";
-import { PRIMARY_COLOR } from "../../utils/constants";
-import useUserStats from "../../hooks/useUserStats";
+import { useEffect, useState } from "react";
 import ElementContainer from "../../components/elementContainer/ElementContainer";
+import RewardsList from "../../layouts/rewardsPage/rewardsList/RewardsList";
+import MyRewardsList from "../../layouts/rewardsPage/myRewardsList/MyRewardsList";
+import Header from "../../components/header/Header";
+import UseRewards from "../../hooks/useRewards";
 import UseMyRewards from "../../hooks/useMyRewards";
-import { reedemReward } from "../../services/RewardsService";
+import useUserStats from "../../hooks/useUserStats";
+import { redeemReward } from "../../services/RewardsService";
+import { BiCoin } from "react-icons/bi";
+import { LuGift, LuX } from "react-icons/lu";
+import { PRIMARY_COLOR, TABLET_WIDTH_SIZE } from "../../utils/constants";
+import "./RewardsPage.css";
 
 const RewardsPage = () => {
+  const { rewards } = UseRewards(["points"]);
+  const { userStats } = useUserStats();
+  const { myRewards } = UseMyRewards();
+  const [showMyRewardsOnMobile, setShowMyRewardsOnMobile] =
+    useState<boolean>(false);
+  const [redeemError, setRedeemError] = useState<boolean>(false);
+  
 
-  const {
-    rewards,
-    isLoading: areRewardsLoading,
-    hasError: hasRewardsError,
-  } = UseRewards(['points']);
-  const {userStats} = useUserStats();
-  const {myRewards} = UseMyRewards();
+  useEffect(() => {
 
-  const handleReedem = async () => {
+    const handleResize = () => {
+        if(window.innerWidth > TABLET_WIDTH_SIZE){
+            setShowMyRewardsOnMobile(false)
+        }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+        window.removeEventListener('resize', handleResize);
+    };
+}, []);
+
+  const handleRedeem = async (id:number) => {
     try {
-      await reedemReward(10);
-
+      await redeemReward(id);
     } catch (error) {
-      console.log('something went wrong!')
+      setRedeemError(true);
     }
+  };
+
+
+  if (showMyRewardsOnMobile) {
+    return (
+      <div className="rewards-page__my-rewards__mobile">
+        <Header className="rewards-page__my-rewards__mobile__header">
+          <h2 className="rewards-page__my-rewards__mobile__header__title">
+            My Rewards
+          </h2>
+          <ElementContainer
+            className="rewards-page__my-rewards__mobile__header__close-button"
+            onClick={() => setShowMyRewardsOnMobile(false)}
+          >
+            <LuX size={32} color={PRIMARY_COLOR} />
+          </ElementContainer>
+        </Header>
+        <MyRewardsList
+          list={myRewards}
+          className="rewards-page__my-rewards__list__mobile"
+        />
+      </div>
+    );
   }
 
   return (
-    <div className="rewards-page__layout">
-      <MenuBar />
-      <div className="rewards-page">
-        {
-          rewards && 
-            <div className="rewards-page__all-rewards">
-              <div className="rewards-page__all-rewards__header">
-                <h2 className="rewards-page__all-rewards__header__title">Rewards</h2>
-                <ElementContainer className="rewards-page__all-rewards__header__reedem-points">
-                  <h2 className="rewards-page__all-rewards__header__reedem-points__count">{userStats?.redeemablePoints}</h2>
-                  <BiCoin size={32} color={PRIMARY_COLOR}/>
-                </ElementContainer>
-              </div>
-              <RewardsList list={rewards} className="rewards-page__all-rewards__list"/>
+    <div className="rewards-page">
+      {rewards && (
+        <div className="rewards-page__all-rewards">
+          <Header className="rewards-page__all-rewards__header">
+            <h2 className="rewards-page__all-rewards__header__title">
+              Rewards
+            </h2>
+            <div className="rewards-page__all-rewards__header__container">
+              <ElementContainer
+                className="rewards-page__all-rewards__header__container__my-rewards-button"
+                onClick={() => setShowMyRewardsOnMobile(true)}
+              >
+                <LuGift size={32} color={PRIMARY_COLOR} />
+              </ElementContainer>
+              <ElementContainer className="rewards-page__all-rewards__header__container__redeem-points">
+                <h2 className="rewards-page__all-rewards__header__container__redeem-points__count">
+                  {userStats?.redeemablePoints}
+                </h2>
+                <BiCoin size={32} color={PRIMARY_COLOR} />
+              </ElementContainer>
             </div>
-        }
-        <div className="rewards-page__my-rewards">
-          <ElementContainer className="rewards-page__my-rewards__header">
-            <h2>My Rewards</h2>
-          </ElementContainer>
-          <RewardsList list={myRewards} className="rewards-page__my-rewards__list" isMyRewardCard reedemablePoints={userStats?.redeemablePoints}/>
+          </Header>
+          <RewardsList
+            list={rewards}
+            className="rewards-page__all-rewards__list"
+            rewardCardRedeemReward={handleRedeem}
+          />
         </div>
+      )}
+      <div className="rewards-page__my-rewards">
+        <ElementContainer className="rewards-page__my-rewards__header">
+          <h2>My Rewards</h2>
+        </ElementContainer>
+        <MyRewardsList
+          list={myRewards}
+          className="rewards-page__my-rewards__list"
+        />
       </div>
     </div>
   );
