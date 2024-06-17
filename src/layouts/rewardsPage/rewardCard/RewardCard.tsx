4d6ef3898ff4a0661ab24d755e2f4fed10e1aa3c
CountMonteCristo13 <buza95@gmail.com>
2024-06-17 08:10:43 +0200

    updated styles for reward card component and corrected typos

--- src/layouts/rewardsPage/rewardCard/RewardCard.tsx
+++ src/layouts/rewardsPage/rewardCard/RewardCard.tsx
@@ -1,61 +1,58 @@
-import { BiCoin, BiGift } from "react-icons/bi";
+import { BiCoin } from "react-icons/bi";
 import { Reward } from "../../../models/RewardsData";
-import { PRIMARY_COLOR } from "../../../utils/constants";
+import { PRIMARY_COLOR, TERTIARY_COLOR } from "../../../utils/constants";
 import Button from "../../../components/button/Button";
 import ElementContainer from "../../../components/elementContainer/ElementContainer";
+import { LuGift } from "react-icons/lu";
 import "./RewardCard.css";
 
 type RewardCardProps = {
-  isMyReward?: boolean;
   reward: Reward;
-  reedmeablePoints?:number;
+  reedmeablePoints?: number;
+  redeemReward: (id:number) => void;
 };
 
 const REWARD_IMAGE_BASE_URL = import.meta.env.VITE_REWARD_IMAGE_BASE_URL;
 
-const RewardCard = ({ reward, isMyReward = false, reedmeablePoints }: RewardCardProps) => {
+const RewardCard = ({ reward, reedmeablePoints, redeemReward }: RewardCardProps) => {
   const imgUrl = REWARD_IMAGE_BASE_URL + reward.imageReference;
+
   return (
     <ElementContainer className="reward-card">
-      <div className={!isMyReward ? 'reward-card__img__container' : 'my-reward-card__img__container'}>
-        {
-          reward.imageReference ?
-            <img src={imgUrl} alt="reward-image" className="reward-card__img" />
-            :
-            <BiGift className="reward-card__img-not-found" />
-        }
+      <div className="reward-card__img__container">
+        {reward.imageReference ? (
+          <img src={imgUrl} alt="reward-image" className="reward-card__img" />
+        ) : (
+          <LuGift
+            className="reward-card__img-not-found"
+            color={TERTIARY_COLOR}
+          />
+        )}
       </div>
 
       <div className="reward-card__description">
-        {
-          isMyReward ?
-            <>
-              <h2 className="my-reward-card__description__reward-name">
-                {reward.name}
-              </h2>
-              <div className="my-reward-card__description__points">
-                <h2 className="my-reward-card__description__points__number">{reward.points}</h2>
-                <BiCoin size={24} color={PRIMARY_COLOR} />
-              </div>
-            </>
-            :
-            <>
-              <div className="reward-card__description__header">
-                <h2 className="reward-card__description__header__title">{reward.name}</h2>
-                <div className="reward-card__description__header__points">
-                  <h2 className="reward-card__description__header__points__number">{reward.points}</h2>
-                  <BiCoin size={32} color={PRIMARY_COLOR} />
-                </div>
-              </div>
-              <p className="reward-card__description__text">{reward.description}</p>
-              <div className="reward-card__footer">
-                <Button title="Reedem" className="reward-card__footer__reedem-button" />
-              </div>
-            </>
-        }
+        <div className="reward-card__description__header">
+          <h2 className="reward-card__description__header__title">
+            {reward.name}
+          </h2>
+        </div>
+        <p className="reward-card__description__text">{reward.description}</p>
+        <div className="reward-card__footer">
+          <div className="reward-card__description__footer__points">
+            <h2 className="reward-card__description__footer__points__number">
+              {reward.points}
+            </h2>
+            <BiCoin size={32} color={PRIMARY_COLOR} className="reward-card__description__footer__points__icon"/>
+          </div>
+          <Button
+            title="Redeem"
+            className="reward-card__footer__redeem-button"
+            onClick={() => redeemReward(reward.id)}
+            disabled={reedmeablePoints! < reward.points}
+          />
+        </div>
       </div>
     </ElementContainer>
-
   );
 };
 
