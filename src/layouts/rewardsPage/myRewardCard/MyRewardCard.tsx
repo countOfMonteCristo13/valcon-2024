f5c3ebc1aeab3ce746a0077db45d8c79b756387a
CountMonteCristo13 <buza95@gmail.com>
2024-06-17 08:08:03 +0200

    created myRewardCard component

---
+++ src/layouts/rewardsPage/myRewardCard/MyRewardCard.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { MyReward } from "../../../models/RewardsData";
+import { PRIMARY_COLOR } from "../../../utils/constants";
+import { LuGift, LuInfo } from "react-icons/lu";
+import Modal from "../../../components/modal/Modal";
+import MyRewardModal from "../myRewardModal/MyRewardModal";
+import "./MyRewardCard.css";
+
+type MyRewardCardProps = {
+  myReward: MyReward;
+};
+
+const MY_REWARD_IMAGE_BASE_URL = import.meta.env.VITE_REWARD_IMAGE_BASE_URL;
+
+const MyRewardCard = ({ myReward }: MyRewardCardProps) => {
+  const imgUrl = MY_REWARD_IMAGE_BASE_URL + myReward.reward.imageReference;
+  const [showMyRewardModal, setShowMyRewardModal] = useState<boolean>(false);
+
+  const closeModal = () => {
+    setShowMyRewardModal(false);
+  }
+
+  const openModal = () => {
+    setShowMyRewardModal(true)
+  }
+
+  return (
+    <div className="my-reward-card" onClick={openModal}>
+      {
+        showMyRewardModal && 
+        <Modal closeModal={closeModal}>
+          <MyRewardModal reward={myReward}/>
+        </Modal>
+      }
+      {myReward.reward.imageReference ? (
+        <img src={imgUrl} alt="reward-image" className="my-reward-card__img"/>
+         ) : (
+          <div className="my-reward-card__img-not-found__wrapper">
+            <LuGift
+              className="my-reward-card__img-not-found"
+              color={PRIMARY_COLOR}
+            />
+          </div>
+        )
+      }
+      <div className="my-reward-card__overlay">
+        <LuInfo size={48} color={PRIMARY_COLOR}/>
+      </div>
+    </div>
+  );
+};
+
+export default MyRewardCard;
