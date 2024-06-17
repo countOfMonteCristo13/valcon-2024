0c809730507ac24a28e7677657dfa2a340a58b83
CountMonteCristo13 <buza95@gmail.com>
2024-06-17 08:01:50 +0200

    updated modal to have close button

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,16 +1,31 @@
 import { ReactNode } from 'react'
+import { LuX } from 'react-icons/lu';
+import { PRIMARY_COLOR } from '../../utils/constants';
 import './Modal.css'
 
 type ModalProps = {
-    children?:ReactNode;
+    children?: ReactNode;
+    closeModal: () => void;
 }
 
-const Modal = ({children}: ModalProps) => {
+const Modal = ({ children, closeModal }: ModalProps) => {
+
+  const handleCloseModal = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    closeModal();
+  };
+
   return (
-    <div className='modal'>
-      {children}
+    <div className='modal' onClick={handleCloseModal}>
+      <div className='modal__body' onClick={(e) => e.stopPropagation()}>
+          <div className='modal__body__close-button' onClick={handleCloseModal}>
+            <LuX size={40} color={PRIMARY_COLOR} />
+          </div>
+          {children}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
+
