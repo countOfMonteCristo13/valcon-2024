48b96fc09aaeb2896a0a8a74478ed95501b88672
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-18 09:05:27 +0200

    changed object-fit for reward images and renamed class names for reward info card

--- src/layouts/rewardsPage/myRewardInfoCard/MyRewardInfoCard.tsx
+++ src/layouts/rewardsPage/myRewardInfoCard/MyRewardInfoCard.tsx
@@ -1,40 +1,30 @@
 import { BiCoin } from 'react-icons/bi';
-import { LuGift } from 'react-icons/lu';
 import { MyReward } from '../../../models/RewardsData'
+import { buildRewardImageURL } from '../../../utils/imageBuilder';
 import './MyRewardInfoCard.css'
 
 type MyRewardInfoCard = {
   reward: MyReward;
 }
 
-const MY_REWARD_IMAGE_BASE_URL = import.meta.env.VITE_REWARD_IMAGE_BASE_URL;
-
 const MyRewardInfoCard = ({reward} : MyRewardInfoCard) => {
-  const imgUrl = MY_REWARD_IMAGE_BASE_URL + reward.reward.imageReference;
 
   return (
-    <div className='my-reward-modal'>
-      <div className="my-reward-modal__img__container">
-        {reward.reward.imageReference ? (
-          <img src={imgUrl} alt="reward-image" className="my-reward-modal__img" />
-        ) : (
-          <LuGift
-            size={250}
-            className="my-reward-modal__img-not-found"
-          />
-        )}
+    <div className='my-reward-info-card'>
+      <div className="my-reward-info-card__img__container">
+        <img src={buildRewardImageURL(reward.reward.imageReference)} alt="reward-image" className='my-reward-info-card__img'/>
       </div>
 
-      <div className="my-reward-modal__description">
-        <div className="my-reward-modal__description__header">
-          <h2 className="my-reward-modal__description__header__title">
+      <div className="my-reward-info-card__description">
+        <div className="my-reward-info-card__description__header">
+          <h2 className="my-reward-info-card__description__header__title">
             {reward.reward.name}
           </h2>
         </div>
-        <p className="my-reward-modal__description__text">{reward.reward.description}</p>
-        <div className="my-reward-modal__footer">
-          <div className="my-reward-modal__description__footer__points">
-            <h2 className="my-reward-modal__description__footer__points__number">
+        <p className="my-reward-info-card__description__text">{reward.reward.description}</p>
+        <div className="my-reward-info-card__footer">
+          <div className="my-reward-info-card__description__footer__points">
+            <h2 className="my-reward-info-card__description__footer__points__number">
               {reward.reward.points}
             </h2>
             <BiCoin size={32} />
