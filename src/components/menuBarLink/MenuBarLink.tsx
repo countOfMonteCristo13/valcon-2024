4582ce60a77e2492ee0106ec2a5c193e716250af
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-19 14:04:18 +0200

    updated when link is active feature

--- src/components/menuBarLink/MenuBarLink.tsx
+++ src/components/menuBarLink/MenuBarLink.tsx
@@ -2,22 +2,28 @@ import { IconType } from "react-icons";
 import { NavLink } from "react-router-dom";
 
 type MenuBarLinkProps = {
-    to: string;
-    linkClassName?:string;
-    icon: IconType;
-    iconSize?:number;
-    linkTitle?: string;
-    linkTitleClassName:string;
+  to: string;
+  linkClassName?: string;
+  activeLinkClassName?: string;
+  icon: IconType;
+  iconSize?: number;
+  linkTitle?: string;
+  linkTitleClassName?: string;
 }
 
-const MenuBarLink = ({to, linkClassName,icon:IconComponent,iconSize=24,linkTitle,linkTitleClassName}: MenuBarLinkProps) => {
+const MenuBarLink = ({ to, linkClassName, icon: IconComponent, iconSize = 24, linkTitle, linkTitleClassName, activeLinkClassName }: MenuBarLinkProps) => {
   return (
-    <NavLink to={to} className={linkClassName}>
-        <IconComponent size={iconSize}/>
-        {
-          linkTitle &&
-        <h3 className={`${linkTitleClassName}`}>{linkTitle}</h3>
-        }
+    <NavLink to={to} className={({ isActive }) =>
+      [
+        linkClassName,
+        isActive ? activeLinkClassName : "",
+      ].join(" ")}
+    >
+      <IconComponent size={iconSize} />
+      {
+        linkTitle &&
+        <p className={`${linkTitleClassName}`}>{linkTitle}</p>
+      }
     </NavLink>
   )
 }
