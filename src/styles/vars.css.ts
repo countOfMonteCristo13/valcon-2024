978d598dd4a2389e642c60211ed05d32f96cdc2e
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-19 10:57:47 +0200

    added global variables for colors

---
+++ src/styles/vars.css.ts
@@ -0,0 +1,9 @@
+import {createGlobalTheme} from '@vanilla-extract/css'
+
+export const vars = createGlobalTheme(':root',{
+    colors:{
+        primary:'#ffae00',
+        secondary:'#3a3a3a',
+        tertiary:'#2b2b2b'
+    }
+})
