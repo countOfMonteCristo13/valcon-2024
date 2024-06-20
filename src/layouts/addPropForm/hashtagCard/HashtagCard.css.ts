6b74d855494da14c829c39525fedac20bf553326
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-06-20 08:11:49 +0200

    applied js in ts to component: HashtagCard

---
+++ src/layouts/addPropForm/hashtagCard/HashtagCard.css.ts
@@ -0,0 +1,11 @@
+import {style} from '@vanilla-extract/css'
+
+export const hashtagCardStyle =style({
+    width:'fit-content',
+    padding:'0.3rem 0.6rem'
+})
+
+export const hashtagCardNameStyle =style({
+    fontSize:'1.2rem',
+    gap:'0.1rem'
+})
