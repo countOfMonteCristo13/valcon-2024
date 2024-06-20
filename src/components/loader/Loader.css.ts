26acb738514d13dd0cb165707214b163fb79f299
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-20 13:17:45 +0200

    updated loader style to accept global variables for colors

--- src/components/loader/Loader.css.ts
+++ src/components/loader/Loader.css.ts
@@ -1,4 +1,5 @@
 import {style, keyframes} from '@vanilla-extract/css'
+import { vars } from '../../styles/vars.css'
 
 const loading = keyframes({
     '100%':{transform:'rotate(1turn)'}
@@ -9,7 +10,7 @@ export const loaderStyle = style({
     width:'50px',
     aspectRatio:'1',
     borderRadius:'50%',
-    background:'radial-gradient(farthest-side,#ffae00 94%,#0000) top/8px 8px no-repeat, conic-gradient(#0000 30%,#ffae00)',
+    background:`radial-gradient(farthest-side,${vars.colors.primary} 94%,#0000) top/8px 8px no-repeat, conic-gradient(#0000 30%,${vars.colors.primary})`,
     WebkitMask:'radial-gradient(farthest-side,#0000 calc(100% - 8px),#000 0)',
     margin:'auto',
     animation:`${loading} 1s infinite linear`,
