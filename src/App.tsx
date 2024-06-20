b31a8230495059c298b4a5fc1c9f55a3a295ef57
Nikola Buzadzija <buzadzija.nikola13@gmail.com>
2024-06-20 13:38:25 +0200

    added theme to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/loginPage/LoginPage";
 import NotFoundPage from "./pages/notFoundPage/NotFoundPage";
@@ -5,22 +6,22 @@ import LandingPage from "./pages/landingPage/LandingPage";
 import RewardsPage from "./pages/rewardsPage/RewardsPage";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 import PropsAppLayout from "./layouts/propsAppLayout/PropsAppLayout";
-import { light,dark } from "./styles/vars.css";
-import { useState } from "react";
+import { light,dark} from "./styles/vars.css";
 
 function App() {
-  const [toggleTheme, setToggleTheme] = useState<boolean>(true);
+  const [theme, setTheme] = useState<boolean>(true);
+
+  const handleToggleTheme = () => {
+    setTheme(!theme);
+  }
 
   return (
-    <main className={toggleTheme ? light : dark}>
-      <div onClick={() => setToggleTheme(!toggleTheme)}>
-        toggle
-      </div>
+    <main className={theme ? light : dark}>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<PropsAppLayout />}>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/rewards" element={<RewardsPage />} />
+          <Route path="/" element={<LandingPage theme={theme} toggleTheme={handleToggleTheme}/>} />
+          <Route path="/rewards" element={<RewardsPage theme={theme} toggleTheme={handleToggleTheme} />}/>
           <Route path="/profile" element={<ProfilePage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
