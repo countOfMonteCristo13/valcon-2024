7d25efa20ae693e225438d3d976af63b6e794a1c
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-06-20 08:10:26 +0200

    updated input form field component styles by adding hide function to number types

--- src/components/inputFormField/InputFormField.tsx
+++ src/components/inputFormField/InputFormField.tsx
@@ -1,9 +1,10 @@
 import { ChangeEvent, HTMLInputTypeAttribute } from 'react';
-import { flexColumn, gap0_5 } from '../../styles/index.css';
+import { flexColumn, fullWidth, gap0_5 } from '../../styles/index.css';
 import { errorBottomBorder, inputFormFieldInputStyle } from './InputFormFiled.css';
 
 type InputFormFieldProps = {
-    id?: string
+    id?: string;
+    fieldClassName?:string;
     label?: string;
     inputType?: HTMLInputTypeAttribute;
     placeholder?: string;
@@ -17,9 +18,9 @@ type InputFormFieldProps = {
 }
 
 
-const InputFormField = ({id,label,inputType = 'text',placeholder,value,onChange,className, onFocus, onBlur,name, hasError = false}: InputFormFieldProps) => {
+const InputFormField = ({id,fieldClassName ,label,inputType = 'text',placeholder,value,onChange,className, onFocus, onBlur,name, hasError = false}: InputFormFieldProps) => {
   return (
-    <div className={`${flexColumn} ${gap0_5}`}>
+    <div className={`${fullWidth} ${flexColumn} ${gap0_5} ${fieldClassName}`}>
       {
         label &&
         <label htmlFor={id}>
@@ -27,7 +28,7 @@ const InputFormField = ({id,label,inputType = 'text',placeholder,value,onChange,
         </label>
       }
         <input 
-          className={`${inputFormFieldInputStyle} ${hasError && errorBottomBorder} ${className}`} 
+          className={`${fullWidth} ${inputFormFieldInputStyle} ${hasError && errorBottomBorder} ${className}`} 
           id={id} type={inputType} name={name} 
           placeholder={placeholder} 
           value={value} 
--- src/components/inputFormField/InputFormFiled.css.ts
+++ src/components/inputFormField/InputFormFiled.css.ts
@@ -5,7 +5,18 @@ export const inputFormFieldInputStyle = style({
     border:'none',
     borderBottom: `3px solid ${vars.colors.primary}`,
     background:'transparent',
-    fontSize: '1rem'
+    fontSize: '1rem',
+    ':focus':{
+        outline:'none'
+    },
+    '::-webkit-inner-spin-button':{
+        WebkitAppearance:'none',
+        margin:0
+    },
+    '::-webkit-outer-spin-button':{
+        WebkitAppearance:'none',
+        margin:0
+    }
 })
 
 export const errorBottomBorder = style({
