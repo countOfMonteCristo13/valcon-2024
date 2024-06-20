4e7e8d46de1f9382ddf76f3b3e4c51db49a02466
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2024-06-20 08:22:38 +0200

    added theme toggle button and theme styles to App component and rearranged routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,24 @@ import LandingPage from "./pages/landingPage/LandingPage";
 import RewardsPage from "./pages/rewardsPage/RewardsPage";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 import PropsAppLayout from "./layouts/propsAppLayout/PropsAppLayout";
+import { light,dark } from "./styles/vars.css";
+import { useState } from "react";
 
 function App() {
+  const [toggleTheme, setToggleTheme] = useState<boolean>(true);
 
   return (
-    <main>
+    <main className={toggleTheme ? light : dark}>
+      <div onClick={() => setToggleTheme(!toggleTheme)}>
+        toggle
+      </div>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<LandingPage />} />
         <Route path="/" element={<PropsAppLayout />}>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/rewards" element={<RewardsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Route>
-        <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </main>
